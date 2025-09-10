41135995d97d0f2942b4b2e1758c499e4be10657
IbtissamB <ibtissam.bahaj@hotmail.com>
2025-09-10 04:19:01 +0300

    interface for preoperty data

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -10,3 +10,22 @@ export interface ButtonProps {
   styles?: string;
   type?: "button" | "submit" | "reset";
 }
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
