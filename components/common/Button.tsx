c49ec8f289e5cc1382ce1b6114da36243f0b7c1f
IbtissamB <ibtissam.bahaj@hotmail.com>
2025-09-11 04:11:05 +0300

    fixed card and button components

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ButtonProps> = ({
-  label,
-  onClick,
-  styles = "",
-  type = "button",
-}) => {
+const Button: React.FC<ButtonProps> = ({ className, onClick, children }) => {
   return (
-    <button
-      type={type}
-      onClick={onClick}
-      className={`px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition ${styles}`}
-    >
-      {label}
+    <button className={className} onClick={onClick}>
+      {children}
     </button>
   );
 };
--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import { CardProps } from "@/interfaces";
 
-const Card: React.FC<CardProps> = ({ title, description, imageUrl }) => {
-  return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300">
-      <img src={imageUrl} alt={title} className="w-full h-48 object-cover" />
-      <div className="p-4">
-        <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
-        <p className="text-sm text-gray-600 mt-2">{description}</p>
-      </div>
-    </div>
-  );
-};
+const Card: React.FC<CardProps> = ({ children, className }: CardProps) => {
+  return <div className={className}>{children}</div>;
+}
 
-export default Card
+export default Card;
